38684d2aba35c4bec12a5cb0eec44807eaab2ed6
DyNamiC <imsong1997@gmail.com>
2020-05-09 16:45:53 +0900

    lec

---
+++ TMapLocation/app/src/main/java/com/example/tmaplocation/CheckPermissionActivity.java
@@ -0,0 +1,18 @@
+package com.example.tmaplocation;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CheckPermissionActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+}
